ea05977b6479df7e7b1bdcfc60b5b894e3084fa6
OPPONG-NKRUMAH David <DOPPONG-NKRUMAH@ghipss.com>
2023-12-28 12:56:31 +0000

    adding enviroment envs

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+import { AppService } from '../app/app.service';
+import { AppComponent } from 'src/app/app.component';
+
+
+//let host="10.1.15.72";
+//let host="localhost";
+let host = '172.27.10.230'
+export const environment = {
+ 
+ 
+//$P{auth_status}.equals("2") ? "Approved" : "Declined" By :
+  production: true,
+  adminUrl:'http://'+host+':8003'
+
+};
